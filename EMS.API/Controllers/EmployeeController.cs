a3e5fb2947b853088668caedb7f6ceb190a43adf
yadavankit667 <54766595+yadavankit667@users.noreply.github.com>
2023-05-02 14:43:30 +0530

    Add files via upload

---
+++ EMS.API/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using EMS.API.Interface;
+using EMS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployee employeeContext;
+
+        public EmployeeController(IEmployee employeeContext)
+        {
+            this.employeeContext = employeeContext;
+        }
+
+        [HttpGet("EmployeeList")]
+        public IActionResult EmployeeList()
+        {
+            return Ok(employeeContext.GetEmployee());
+        }
+
+        [HttpPost("SaveEmployee")]
+        public IActionResult SaveEmployee(Employee employee)
+        {
+            return Ok(employeeContext.SaveEmployee(employee));
+        }
+
+        [HttpPut("UpdateEmployee")]
+        public IActionResult UpdateEmployee(Employee employee, int id)
+        {
+            return Ok(employeeContext.UpdateEmployee(employee, id));
+        }
+
+        [HttpDelete("DeleteEmployee")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            return Ok(employeeContext.DeleteEmployee(id));
+        }
+
+        [HttpGet("GetEmployeeById")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            return Ok(employeeContext.GetEmployeeById(id));
+        }
+
+        [HttpGet("SearchEmployee")]
+        public IActionResult SearchEmployee(string searchText)
+        {
+            return Ok(employeeContext.SearchEmployee(searchText));
+        }
+    }
+}
